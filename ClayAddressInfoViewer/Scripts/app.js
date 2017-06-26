4277932709d8bd6c450bc649737f3a4d20bcf03e
Jeremy West <jeremy.west@claycountygov.com>
2017-06-26 14:06:16 -0400

    Added app.ts and created function frameworks to create lists and list items.

---
+++ ClayAddressInfoViewer/Scripts/app.js
@@ -0,0 +1,30 @@
+/// <reference path="typings/bootstrap/index.d.ts" />
+/// <reference path="typings/jquery/jquery.d.ts" />
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function CreateLabel() {
+    var label = document.createElement("label");
+    return label;
+}
+exports.CreateLabel = CreateLabel;
+function CreateHeading() {
+    var heading = document.createElement("h2");
+    return heading;
+}
+exports.CreateHeading = CreateHeading;
+function CreateSubHeading() {
+    var subheading = document.createElement("h4");
+    return subheading;
+}
+exports.CreateSubHeading = CreateSubHeading;
+function CreateUnorderedList() {
+    var ulist = document.createElement("ul");
+    return ulist;
+}
+exports.CreateUnorderedList = CreateUnorderedList;
+function CreateUnorderdedListItem() {
+    var litem = document.createElement("li");
+    return null;
+}
+exports.CreateUnorderdedListItem = CreateUnorderdedListItem;
+//# sourceMappingURL=app.js.map
---
+++ ClayAddressInfoViewer/Scripts/app.ts
@@ -0,0 +1,47 @@
+﻿/// <reference path="typings/bootstrap/index.d.ts" />
+/// <reference path="typings/jquery/jquery.d.ts" />
+
+
+
+"use strict"
+
+export function CreateLabel(): HTMLLabelElement
+{
+  let label: HTMLLabelElement = (<HTMLLabelElement>document.createElement("label"));
+  return label;
+}
+
+export function CreateHeading(): HTMLHeadingElement
+{
+  let heading: HTMLHeadingElement = (<HTMLHeadingElement>document.createElement("h2"));
+
+
+  return heading;
+}
+
+export function CreateSubHeading(): HTMLHeadingElement
+{
+  let subheading: HTMLHeadingElement = (<HTMLHeadingElement>document.createElement("h4"));
+
+
+  return subheading;
+}
+
+export function CreateUnorderedList(): HTMLUListElement
+{
+  let ulist: HTMLUListElement = (<HTMLUListElement>document.createElement("ul"));
+
+
+  return ulist;
+}
+
+export function CreateUnorderdedListItem(): HTMLLIElement
+{
+  let litem: HTMLLIElement = (<HTMLLIElement>document.createElement("li"));
+
+
+  return null;
+}
+
+
+
